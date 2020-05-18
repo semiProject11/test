fcdbdfdd21d2ac16b9d75ca9d0e630285290cfb0
user2 <user2@DESKTOP-54MEH4L>
2020-05-18 19:34:56 +0900

    지나가던 조원2가 남김

--- test/src/test/Test.java
+++ test/src/test/Test.java
@@ -4,7 +4,8 @@ public class Test {
 
 	public static void main(String[] args) {
 		System.out.println("안녕 우리조?");
-		System.out.println("난 누굴까 ");
+		System.out.println("난 누굴까  ");
+		System.out.println("1차시도 실패 2차 도전");
 	}
 
 }
