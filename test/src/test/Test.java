8506d4b550cad8e4e1dc0854fb97541e374c0ce9
user2 <user2@DESKTOP-54MEH4L>
2020-05-18 19:29:04 +0900

    지니가던 조원2가 한줄 남겨놓고 띄어쓰기함

--- test/src/test/Test.java
+++ test/src/test/Test.java
@@ -4,7 +4,7 @@ public class Test {
 
 	public static void main(String[] args) {
 		System.out.println("¾È³ç ¿ì¸®Á¶?");
-		System.out.println("³­ ´©±¼±î");
+		System.out.println("³­ ´©±¼±î ");
 	}
 
 }
